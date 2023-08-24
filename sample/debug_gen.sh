c1490d86756b7ed946ef6a53eeeb117eadf98099
tenk-a <tenka@6809.net>
2023-08-25 02:52:32 +0900

    sample修正

--- sample/debug_gen.sh
+++ sample/debug_gen.sh
@@ -1,1 +1,5 @@
-../build/cnvToPdf -o="tmp_テスト.pdf" -t="タイトル♠" -a="著者" data/*.jpg
+../build/cnvToPdf -o="tmp_テスト0.pdf" -t="タイトル♠" -a="著者" data/*.jpg
+../build/cnvToPdf -t="タイトル♠1" -a="著者" data/*.jpg
+../build/cnvToPdf -T="[著者] タイトル♠2" data/*.jpg
+../build/cnvToPdf -T="著者 - タイトル♠3" data/*.jpg
+../build/cnvToPdf -D- -a="著者" -t="タイトル♠1-D-" data/*.jpg
