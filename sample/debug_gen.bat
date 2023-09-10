33a545126a7e1727d68b49f2695ad509e907ef2a
tenk-a <tenka@6809.net>
2023-09-11 02:52:15 +0900

    Win で UTF8 API を使う設定追加.

--- sample/debug_gen.bat
+++ sample/debug_gen.bat
@@ -1,6 +1,6 @@
 chcp 65001
 ..\build\debug\cnvToPdf.exe -otmp_テスト0.pdf -t="タイトル♠" -a="著者" data/*.jpg
-..\build\debug\cnvToPdf.exe -t="タイトル1" -a="著者" data/*.jpg
+..\build\debug\cnvToPdf.exe -t="タイトル1♥" -a="著者" data/*.jpg
 ..\build\debug\cnvToPdf.exe -T="[著者] タイトル2" data/*.jpg
 ..\build\debug\cnvToPdf.exe -T="著者 - タイトル3" data/*.jpg
 ..\build\debug\cnvToPdf.exe -D- -a="著者" -t="タイトル1-D-" data/*.jpg
